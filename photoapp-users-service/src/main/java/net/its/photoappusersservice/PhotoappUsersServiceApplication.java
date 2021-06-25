1092129f1c593e315a404ab41fe1b9cb6b021e41
SOvsyankin <sovsyankin@arlo.com>
2021-06-26 04:02:04 +0600

    Add user microservice

---
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/PhotoappUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package net.its.photoappusersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappUsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappUsersServiceApplication.class, args);
+    }
+
+}
---
+++ photoapp-users-service/src/test/java/net/its/photoappusersservice/PhotoappUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.its.photoappusersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappUsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
