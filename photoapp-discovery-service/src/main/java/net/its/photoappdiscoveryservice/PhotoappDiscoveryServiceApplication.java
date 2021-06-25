758aef026933928451f06ac8b7493f8fce4f215e
SOvsyankin <sovsyankin@arlo.com>
2021-06-26 03:46:55 +0600

    Initial commit

---
+++ photoapp-discovery-service/src/main/java/net/its/photoappdiscoveryservice/PhotoappDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package net.its.photoappdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ photoapp-discovery-service/src/test/java/net/its/photoappdiscoveryservice/PhotoappDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.its.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
