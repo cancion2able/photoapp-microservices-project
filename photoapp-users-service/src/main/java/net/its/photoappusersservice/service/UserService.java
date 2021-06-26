defc8e42f45422a9cd5e9047e7f740288081f9f0
SOvsyankin <sovsyankin@arlo.com>
2021-06-27 00:46:34 +0600

    [users-service] Create shared DTO class

--- photoapp-users-service/src/main/java/net/its/photoappusersservice/service/UserService.java
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/service/UserService.java
@@ -1,12 +1,6 @@
-/**
- * Copyright (c) 2021, by Arlo Technologies, Inc. All rights reserved.
- *
- * This software is the confidential and proprietary information of Arlo Technologies, Inc.
- * ("Confidential Information"). You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement you entered into with
- * Arlo Technologies, Inc.
- */
 package net.its.photoappusersservice.service;
+
+import net.its.photoappusersservice.shared.UserDto;
 
 public interface UserService {
 
