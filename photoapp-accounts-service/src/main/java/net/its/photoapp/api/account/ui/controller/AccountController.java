71b90da12d02d4dd232932db4e53879d71f88ade
SOvsyankin <sovsyankin@arlo.com>
2021-06-26 14:48:59 +0600

    Delete file templates

--- photoapp-accounts-service/src/main/java/net/its/photoapp/api/account/ui/controller/AccountController.java
+++ photoapp-accounts-service/src/main/java/net/its/photoapp/api/account/ui/controller/AccountController.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2021, by Arlo Technologies, Inc. All rights reserved.
- *
- * This software is the confidential and proprietary information of Arlo Technologies, Inc.
- * ("Confidential Information"). You shall not disclose such Confidential Information and
- * shall use it only in accordance with the terms of the license agreement you entered into with
- * Arlo Technologies, Inc.
- */
 package net.its.photoapp.api.account.ui.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
