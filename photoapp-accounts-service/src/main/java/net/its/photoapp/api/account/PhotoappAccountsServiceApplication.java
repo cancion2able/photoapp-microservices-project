9a4a46a87812df33a3fb5502e9d0d831c9986b01
SOvsyankin <sovsyankin@arlo.com>
2021-06-26 14:39:36 +0600

    [albums-service] Setup eureka client

--- photoapp-accounts-service/src/main/java/net/its/photoapp/api/account/PhotoappAccountsServiceApplication.java
+++ photoapp-accounts-service/src/main/java/net/its/photoapp/api/account/PhotoappAccountsServiceApplication.java
@@ -2,8 +2,10 @@ package net.its.photoapp.api.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhotoappAccountsServiceApplication {
 
     public static void main(String[] args) {
