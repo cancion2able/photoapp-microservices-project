999cb6e24a19bc8576df00fef581c1b50ad13fca
SOvsyankin <sovsyankin@arlo.com>
2021-06-26 15:25:38 +0600

    [api-gateway] Create api gateway

---
+++ photoapp-api-gateway/src/main/java/net/its/photoapp/api/gateway/PhotoappApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package net.its.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappApiGatewayApplication.class, args);
+    }
+
+}
