1389cd0df38255650c8b7bb493f298e379d345ac
SOvsyankin <sovsyankin@arlo.com>
2021-06-26 14:36:52 +0600

    [albums-service] Add new microservice

---
+++ photoapp-accounts-service/src/main/java/net/its/photoapp/api/account/PhotoappAccountsServiceApplication.java
@@ -0,0 +1,13 @@
+package net.its.photoapp.api.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappAccountsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappAccountsServiceApplication.class, args);
+    }
+
+}
---
+++ photoapp-accounts-service/src/test/java/net/its/photoapp/api/account/PhotoappAccountsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.its.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAccountsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
