83ba855f041da8da14fa634e4211a448ec20d2f1
SOvsyankin <sovsyankin@arlo.com>
2021-06-27 00:48:58 +0600

    [users-service] Generate unique public user id

--- photoapp-users-service/src/main/java/net/its/photoappusersservice/service/DefaultUserService.java
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/service/DefaultUserService.java
@@ -2,10 +2,13 @@ package net.its.photoappusersservice.service;
 
 import net.its.photoappusersservice.shared.UserDto;
 
+import java.util.UUID;
+
 public class DefaultUserService implements UserService {
 
     @Override
     public UserDto createUser(UserDto userDetails) {
+        userDetails.setUserId(UUID.randomUUID().toString());
         return null;
     }
 }
