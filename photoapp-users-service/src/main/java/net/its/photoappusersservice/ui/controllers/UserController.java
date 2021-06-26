c3356993f918246e42dc39ba739ce7382cca1ada
SOvsyankin <sovsyankin@arlo.com>
2021-06-27 00:36:34 +0600

    [users-service] Validate HTTP request body

--- photoapp-users-service/src/main/java/net/its/photoappusersservice/ui/controllers/UserController.java
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/ui/controllers/UserController.java
@@ -4,6 +4,8 @@ import net.its.photoappusersservice.ui.model.CreateUserRequestModel;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -20,7 +22,7 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
         return "CreateUser method is called";
     }
 }
--- photoapp-users-service/src/main/java/net/its/photoappusersservice/ui/model/CreateUserRequestModel.java
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/ui/model/CreateUserRequestModel.java
@@ -8,11 +8,24 @@
  */
 package net.its.photoappusersservice.ui.model;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class CreateUserRequestModel {
 
+    @NotNull(message = "First name cannot be null")
+    @Size(min = 2, message = "First name must not be less than 2 characters")
     private String firstName;
+    @NotNull(message = "Last name cannot be null")
+    @Size(min = 2, message = "Last name must not be less than 2 characters")
     private String lastName;
+    @NotNull(message = "Password name cannot be null")
+    @Size(min = 8, max = 16,
+          message = "Password must be equal or greater than 8 characters and less than 16 characters")
     private String password;
+    @NotNull(message = "Email name cannot be null")
+    @Email
     private String email;
 
     public String getFirstName() {
