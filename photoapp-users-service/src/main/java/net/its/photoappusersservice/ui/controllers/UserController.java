0b32494591d574f0797f2bdc38686fdd1f0edee0
SOvsyankin <sovsyankin@arlo.com>
2021-06-27 00:28:36 +0600

    [users-service] Add placeholder for user creation

--- photoapp-users-service/src/main/java/net/its/photoappusersservice/ui/controllers/UserController.java
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/ui/controllers/UserController.java
@@ -2,6 +2,7 @@ package net.its.photoappusersservice.ui.controllers;
 
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,10 @@ public class UserController {
     @GetMapping("/status/check")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "CreateUser method is called";
     }
 }
