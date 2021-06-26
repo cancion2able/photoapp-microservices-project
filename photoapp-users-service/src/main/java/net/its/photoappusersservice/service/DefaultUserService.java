e5b5ac266d577844a9a49e2fb392bea23bc6826b
SOvsyankin <sovsyankin@arlo.com>
2021-06-27 00:41:47 +0600

    [users-service] Implement service layer class

---
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/service/DefaultUserService.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2021, by Arlo Technologies, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Arlo Technologies, Inc.
+ * ("Confidential Information"). You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement you entered into with
+ * Arlo Technologies, Inc.
+ */
+package net.its.photoappusersservice.service;
+
+public class DefaultUserService implements UserService {
+
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        return null;
+    }
+}
