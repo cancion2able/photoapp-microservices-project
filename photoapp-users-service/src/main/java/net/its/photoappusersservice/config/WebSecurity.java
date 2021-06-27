370cad9c166b81a78089d2b1cc235e2bdbb3373e
SOvsyankin <sovsyankin@arlo.com>
2021-06-27 21:44:45 +0600

    [users-service] Configure spring security

---
+++ photoapp-users-service/src/main/java/net/its/photoappusersservice/config/WebSecurity.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2021, by Arlo Technologies, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Arlo Technologies, Inc.
+ * ("Confidential Information"). You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement you entered into with
+ * Arlo Technologies, Inc.
+ */
+package net.its.photoappusersservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/users/**")
+                .permitAll();
+    }
+}
