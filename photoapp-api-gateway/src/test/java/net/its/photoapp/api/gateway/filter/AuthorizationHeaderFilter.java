5168d308139822140feec3cc044a52986c356184
SOvsyankin <sovsyankin@arlo.com>
2021-07-03 23:50:34 +0600

    [api-gateway] Create authorization filter

---
+++ photoapp-api-gateway/src/test/java/net/its/photoapp/api/gateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2021, by Arlo Technologies, Inc. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Arlo Technologies, Inc.
+ * ("Confidential Information"). You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement you entered into with
+ * Arlo Technologies, Inc.
+ */
+package net.its.photoapp.api.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    public static class Config {
+        // Put configuration properties here
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return null;
+    }
+}
