1a7335bb74f081be8dce4b85a6305dcdc0be87b6
SOvsyankin <sovsyankin@arlo.com>
2021-07-04 16:06:00 +0600

    [api-gateway] Access request path and HTTP headers

--- photoapp-api-gateway/src/main/java/net/its/photoapp/api/gateway/filter/DefaultPreFilter.java
+++ photoapp-api-gateway/src/main/java/net/its/photoapp/api/gateway/filter/DefaultPreFilter.java
@@ -4,9 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Set;
 
 @Component
 public class DefaultPreFilter implements GlobalFilter {
@@ -16,6 +19,14 @@ public class DefaultPreFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         logger.info("A default pre-filter is executed...");
+        final String requestPath = exchange.getRequest().getPath().value();
+        logger.info("Request path = {}", requestPath);
+        final HttpHeaders headers = exchange.getRequest().getHeaders();
+        final Set<String> headerNames = headers.keySet();
+        headerNames.forEach(headerName -> {
+            final String headerValue = headers.getFirst(headerName);
+            logger.info("{}: {}", headerName, headerValue);
+        });
         return chain.filter(exchange);
     }
 }
