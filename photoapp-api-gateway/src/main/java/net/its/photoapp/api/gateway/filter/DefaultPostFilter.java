351d1473998fa3c56bfaa702a33e38f5b0e2f746
SOvsyankin <sovsyankin@arlo.com>
2021-07-04 16:18:19 +0600

    [api-gateway] Create global post-filter

---
+++ photoapp-api-gateway/src/main/java/net/its/photoapp/api/gateway/filter/DefaultPostFilter.java
@@ -0,0 +1,21 @@
+package net.its.photoapp.api.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class DefaultPostFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(DefaultPostFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange)
+                .then(Mono.fromRunnable(() -> logger.info("Global post-filter is executed.......")));
+    }
+}
