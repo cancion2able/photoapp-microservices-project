3a5177a5f6dd4740c15a1d014793d84963075784
SOvsyankin <sovsyankin@arlo.com>
2021-07-04 16:28:36 +0600

    [api-gateway] Define filters in a single class

---
+++ photoapp-api-gateway/src/main/java/net/its/photoapp/api/gateway/filter/GlobalFiltersConfiguration.java
@@ -0,0 +1,41 @@
+package net.its.photoapp.api.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GlobalFiltersConfiguration {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalFiltersConfiguration.class);
+
+    @Bean
+    public GlobalFilter secondPreFilter() {
+        return (exchange, chain) -> {
+            logger.info("My second global pre-filter is executed...");
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> logger.info("My second global post-filter is executed...")));
+        };
+    }
+
+    @Bean
+    public GlobalFilter thirdPreFilter() {
+        return (exchange, chain) -> {
+            logger.info("My third global pre-filter is executed...");
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> logger.info("My third global post-filter is executed...")));
+        };
+    }
+
+    @Bean
+    public GlobalFilter fourthPreFilter() {
+        return (exchange, chain) -> {
+            logger.info("My fourth global pre-filter is executed...");
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> logger.info("My fourth global post-filter is executed...")));
+        };
+    }
+}
