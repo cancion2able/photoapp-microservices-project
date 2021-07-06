32850a2411f63d1667644ac16779f0dce4d6fd9b
SOvsyankin <sovsyankin@arlo.com>
2021-07-06 15:39:14 +0600

    [config-server] Add config server project

---
+++ photoapp-config-server/src/test/java/net/its/photoapp/api/PhotoappConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.its.photoapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
